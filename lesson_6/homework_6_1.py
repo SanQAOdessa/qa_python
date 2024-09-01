12ea7c1e6c1897b07a1f99632c09e6476b185781
SanQAOdessa <50326402+SanQAOdessa@users.noreply.github.com>
2024-09-01 13:11:03 +0300

    Update lesson_6/homework_6_1.py
    Co-authored-by: Eugene Danilovets <eugene.danilovets@gmail.com>

--- lesson_6/homework_6_1.py
+++ lesson_6/homework_6_1.py
@@ -8,4 +8,4 @@ for symbol in all_symbols_in_my_input:
     if my_input.count(symbol) == 1:
         unique_symbols_counter += 1
 
-print(True if unique_symbols_counter>10 else False)
+print(True if unique_symbols_counter > 10 else False)
